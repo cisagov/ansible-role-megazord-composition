157ecf107b824e50d2fdb4e4fc837bf5a46ea35d
bhambleton <brianthomashambleton@gmail.com>
2022-06-30 15:56:49 -0400

    Adds test for out.profile and .htaccess

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -12,7 +12,13 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize("x", [True])
-def test_packages(host, x):
-    """Run a dummy test, just to show what one would look like."""
-    assert x
+@pytest.mark.parametrize(
+    "f", ["/tools/SourcePoint/out.profile", "/tools/cs2modrewrite/.htaccess"]
+)
+def test_files(host, f):
+    """Test that expected files were created and are non-empty."""
+    assert host.file(f).exists
+    assert host.file(f).is_file
+    assert host.file(f).content
+    if f == ".htaccess":
+        assert "[P,L]" not in host.file(f).content_string
