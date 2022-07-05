07341854de1e306d18eac1afec2651583656e10a
bhambleton <brianthomashambleton@gmail.com>
2022-07-05 08:28:15 -0400

    fix linting errors

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,7 +1,8 @@
 """Module containing the tests for the default scenario."""
 
 # Standard Python Libraries
-import os, re
+import os
+import re
 
 # Third-Party Libraries
 import pytest
@@ -12,30 +13,27 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-@pytest.mark.parametrize(
-    "f", ["/tools/cs2modrewrite/.htaccess"]
-)
+@pytest.mark.parametrize("f", ["/tools/cs2modrewrite/.htaccess"])
 def test_htaccess_file(host, f):
     """Test that expected htaccess file was created and is non-empty."""
-    
     assert host.file(f).exists
     assert host.file(f).is_file
     assert host.file(f).content
     assert "[P,L]" not in host.file(f).content_string
 
+
 @pytest.mark.parametrize("d", ["/tools/SourcePoint"])
 def test_sourcepoint_profile(host, d):
-    """Test that the expected sourcepoint profile was created and is not empty"""
-
+    """Test that the expected sourcepoint profile was created and is not empty."""
     # First test if SourcePoint directory exists
     assert host.file(d).exists
     assert host.file(d).is_directory
     dirContents = host.file(d).listdir()
     assert dirContents
 
-    # Iterate of directory contents to ensure at least 1 file is present 
+    # Iterate of directory contents to ensure at least 1 file is present
     # matching the scheme below
-    scheme = "SourcePoint-\d{4}-\d{2}-\d{2}.profile"
+    scheme = r"SourcePoint-\d{4}-\d{2}-\d{2}.profile"
     match = [file for file in dirContents if re.match(scheme, file)]
     assert len(match) > 0
     assert host.file(match[0]).exists
